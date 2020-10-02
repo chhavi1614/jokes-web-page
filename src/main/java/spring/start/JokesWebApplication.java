922887e2c683d64d1429b126e0ac96b384583899
chhavi1614 <chhavi.jain.mat17@itbhu.ac.in>
2020-10-02 22:31:28 +0530

    Initial commit

---
+++ src/main/java/spring/start/JokesWebApplication.java
@@ -0,0 +1,13 @@
+package spring.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesWebApplication.class, args);
+	}
+
+}
